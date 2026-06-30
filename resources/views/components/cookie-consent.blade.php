7e29e3d72dafce0cc51ff80ebd30fc47ed414cce
VincT89 <vincenzotito@icloud.com>
2026-06-30 14:35:56 +0200

    .

--- resources/views/components/cookie-consent.blade.php
+++ resources/views/components/cookie-consent.blade.php
@@ -3,8 +3,8 @@
         <h4 class="cookie-banner-title">{{ __('legal.cookie_banner_title') }}</h4>
         <p class="cookie-banner-text">{{ __('legal.cookie_banner_text') }} <a href="{{ url('/' . app()->getLocale() . '/cookie') }}" class="cookie-link">{{ __('legal.cookie') }}</a></p>
         <div class="cookie-actions">
-            <button class="cookie-btn cookie-btn-accept" id="cookieAcceptBtn">{{ __('legal.accept_external') }}</button>
-            <button class="cookie-btn cookie-btn-reject" id="cookieRejectBtn">{{ __('legal.reject_external') }}</button>
+            <button type="button" class="cookie-btn cookie-btn-accept" id="cookieAcceptBtn">{{ __('legal.accept_external') }}</button>
+            <button type="button" class="cookie-btn cookie-btn-reject" id="cookieRejectBtn">{{ __('legal.reject_external') }}</button>
         </div>
     </div>
 </div>
